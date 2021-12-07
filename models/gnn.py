d8ff24df9c8c3bab8531533a5cb66fa4d6d27061
Max Daniels <daniels.g@northeastern.edu>
2021-12-07 16:51:32 +0100

    - Alternative impl of ClassConditionalBias as SpikeEncoder (only for testing convenience, these should be merged) - Other impl: MLPEncoder, MLPDecoder, GraphConvLayer

--- models/gnn.py
+++ models/gnn.py
@@ -1,20 +1,48 @@
 import torch
-
+from models.simple import ReluFCN
+from models.graph import StateGraph
 
 # This will do the propagation step, details TBD
-class GraphConvGNN(torch.nn.Module):
-    def __init__(self):
-        super(GraphConvGNN, self).__init__()
+class GraphConvLayer(torch.nn.Module):
+    def __init__(self, state_dim, layer_depth=1, n_layers=1):
+        super(GraphConvLayer, self).__init__()
+        self.state_dim = state_dim
+        self.fcn = ReluFCN(layers=[3 * state_dim] + layer_depth * [state_dim], batchnorm=True)
+        self.linear_output = torch.nn.Linear(state_dim, state_dim)
+        self.n_layers = n_layers
 
-    def forward(self, v, neighborhood):
+    def local_update(self, present_state, past_state, neighborhood_states, neighborhood_edge_weights):
         '''
         Update the state vector of a single node.
 
-        :param v: state vector v_i of a given node i
-        :param neighborhood: list of tuples [(e_{ij}, v_j) for neighbors v_j of v]. Each tuple contains a real positive
-            valued edge weight e_{ij} and a neighboring state vector v_j.
+        :param present_state: state vector node i at time t
+        :param past_state: state vector node i at time t-1
+        :param neighborhood_states: a matrix [ # of neighbors, state_dim ] containing state vectors for each neighbor
+        :param neighborhood_edge_weights: a vector [ # of neighbors, ] containing edge weights
         :return: updated state vector of v_i
         '''
+        partition = torch.sum(neighborhood_edge_weights)
+        local_avg = torch.matmul(neighborhood_edge_weights.view([1, -1])/partition, neighborhood_states)
+        inp = torch.cat([present_state, past_state, local_avg], dim=0)
+        return self.linear_output(self.fcn(inp))
 
-    def global_update(self, state_graph):
-        ...
+    def forward(self, state_graph):
+        '''
+        Given a state graph at time t, compute states for all vertices at time t+1 and incremement the state graph.
+        :param state_graph:
+        :return:
+        '''
+
+        T, N, dh = state_graph.t_time, state_graph.n_vertices, state_graph.state_dim
+        ew = state_graph.edge_weights()
+        sum_neighbor_state = torch.bmm(ew, state_graph.node_data())
+        Z = torch.sparse.sum(ew, dim=2).to_dense().view((T, N, 1))
+        Z[Z==0] = 1
+        avg_neighbor_state = sum_neighbor_state / Z
+
+        prev_state = torch.cat([torch.zeros((1, N, dh)), state_graph.node_data()[1:]], dim=0)
+
+        gnn_inputs = torch.cat([state_graph.node_data(), prev_state, avg_neighbor_state], dim=2).reshape((T * N, 3*dh))
+        new_state = self.fcn(gnn_inputs).reshape((T, N, dh))
+        return StateGraph(N, T, new_state, state_graph.edge_weights())
+
--- models/graph.py
+++ models/graph.py
@@ -1,20 +1,25 @@
 import numpy as np
+import torch
+from torch import nn
+from models.simple import ReluFCN
 
 class StateGraph():
     def __init__(self, n_vertices, t_time, node_data, edge_weights):
         '''
         Represents the state of a population of `n_vertices` people over `t_time` timesteps.
 
+        Note: for fixed time t, the ith row corresponds to contacts of vertex i
         :param n_vertices: number of vertices
-        :param t_time: number of timesteps
+        :param t_time: number of timesteps (ie. size of window used for classification)
         :param node_data: a matrix of shape (T, N, d_h) of d_h-dimensional state vectors per vertex
-        :param edge_weights: a csr sparse matrix C of shape (T, N, N) representing adjacencies in the contact graph. If
+        :param edge_weights: a coo sparse tensor C of shape (T, N, N) representing adjacencies in the contact graph. If
             persons (i, j) are in contact at time t then C_{t, i, j} > 0.
         '''
         self.n_vertices = n_vertices
         self.t_time = t_time
-        self.node_data = node_data
-        self.edge_weights = edge_weights
+        self.state_dim = node_data.size()[-1]
+        self._node_data = node_data
+        self._edge_weights = edge_weights.coalesce()
 
     def local_state(self, t, n):
         '''
@@ -23,12 +28,12 @@ class StateGraph():
         :param t: time index of requested node
         :return: (present_node_data, past_node_data, [node_data for each neighbor], [edge weight for each neighbor])
         '''
-        present_node_data = self.node_data[t, n]
+        present_node_data = self._node_data[t, n]
         if(t > 0):
-            past_node_data = self.node_data[t - 1, n]
-            neighbors = self.edge_weights[t - 1, n].nonzero()
-            neighboring_data = self.node_data[t - 1, neighbors]
-            neighboring_edge_weights = self.edge_weights[t - 1, n, neighbors]
+            past_node_data = self._node_data[t-1, n]
+            neighbors = self._edge_weights[t-1, n].coalesce()
+            neighboring_data = self._node_data[t - 1, neighbors.indices().flatten()]
+            neighboring_edge_weights = neighbors.values()
         else:
             past_node_data = np.zeros_like(present_node_data)
             neighboring_data = np.array([])
@@ -45,7 +50,62 @@ class StateGraph():
         :return:
         '''
 
-        node_data = np.stack([self.node_data[1:], node_data], axis=0)
-        edge_weights = np.stack([self.edge_weights[1:], contacts], axis=0)
+        node_data = np.stack([self._node_data[1:], node_data], axis=0)
+        edge_weights = np.stack([self._edge_weights[1:], contacts], axis=0)
 
         return StateGraph(self.n_vertices, self.t_time, node_data, edge_weights)
+
+    def normalized_adjacencies(self):
+        row_sums = self._edge_weights.sum(axis=2)
+        return self._edge_weights * (1/row_sums).reshape((1, -1, 1))
+
+    def node_data(self):
+        return torch.clone(self._node_data)
+
+    def edge_weights(self):
+        return torch.clone(self._edge_weights)
+
+class SpikeEncoder(nn.Module):
+    def __init__(self, n_vertices, t_timesteps, state_dim):
+        super(SpikeEncoder, self).__init__()
+        self.n_vertices = n_vertices
+        self.t_timesteps = t_timesteps
+        self.state_dim = state_dim
+        self.pos_test_spike = nn.Parameter(torch.FloatTensor(np.random.normal(size=(state_dim,), scale=1/np.sqrt(state_dim))))
+        self.neg_test_spike = nn.Parameter(torch.FloatTensor(np.random.normal(size=(state_dim,), scale=1/np.sqrt(state_dim))))
+
+    def forward(self, state_graph, observations):
+        spikes = torch.zeros((self.t_timesteps, self.n_vertices, self.state_dim))
+        spikes[observations == -1, ...] += self.neg_test_spike
+        spikes[observations == 1, ...] += self.pos_test_spike
+        new_node_data = state_graph.node_data() + spikes
+        return StateGraph(self.n_vertices, self.t_timesteps, new_node_data, state_graph.edge_weights())
+
+class MLPEncoder(nn.Module):
+    def __init__(self, n_vertices, t_timesteps, layers):
+        super(MLPEncoder, self).__init__()
+        self.n_vertices = n_vertices
+        self.t_timesteps = t_timesteps
+        self.pos_mlp = ReluFCN(layers=layers)
+        self.neg_mlp = ReluFCN(layers=layers)
+
+    def forward(self, state_graph, observations):
+        new_node_data = state_graph.node_data()
+        new_node_data[observations == -1, ...] = self.neg_mlp(new_node_data[observations == -1, ...])
+        new_node_data[observations == 1, ...] = self.pos_mlp(new_node_data[observations == 1, ...])
+        return StateGraph(self.n_vertices, self.t_timesteps, new_node_data, state_graph.edge_weights)
+
+class MLPDecoder(nn.Module):
+    def __init__(self, n_vertices, t_timesteps, layers, state_dim):
+        super(MLPDecoder, self).__init__()
+        assert layers[0] == t_timesteps * state_dim, "First layer must have input dimension T*state_dim"
+        assert layers[-1] == state_dim, "Last layer must have output dimension state_dim"
+        self.n_vertices = n_vertices
+        self.t_timesteps = t_timesteps
+        self.decoder = ReluFCN(layers=layers)
+
+    def forward(self, state_graph):
+        node_data = state_graph.node_data()
+        T, N, dh = node_data.size()
+        node_data = node_data.transpose(1, 0).reshape((N, T * dh))
+        return self.decoder(node_data)
